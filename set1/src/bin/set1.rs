af156cea56c0a5fc9a8347b6bef1be98563b1319
f3r10 <frledesma@outlook.com>
2024-03-18 11:36:24 -0500

    refactor

--- set1/src/bin/set1.rs
+++ set1/src/bin/set1.rs
@@ -1,5 +1,12 @@
-use set1::base64::hex_to_base64;
+use set1::{base64::hex_to_base64, single_byte_xor_cipher::decipher_message};
 
 fn main() {
     println!("hex_to_base64: {}", hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"));
+
+    let msg_hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let (decrypted_msg, score) = decipher_message(msg_hex);
+    println!(
+        "decipher_message {:?} with score {:?}",
+        decrypted_msg, score
+    );
 }
